afbc9e12f92e4ccd6bef000f4eedc964feb2d7a2
Andreas de Ruiter <andreasderuiter@andreass-mac-mini.home>
2026-03-13 22:59:19 +0100

    chore: update barrel exports for connector simplification

--- packages/connectors/src/index.ts
+++ packages/connectors/src/index.ts
@@ -1,15 +1,9 @@
 // Service interfaces (common -- environment-agnostic)
-export { IConnectorRegistry } from './common/connectorRegistry.js';
 export { IMCPClientManager } from './common/mcpClientManager.js';
 export type { ToolInfo } from './common/mcpClientManager.js';
-export { ICLIDetectionService } from './common/cliDetection.js';
-export type { CLIToolStatus } from './common/cliDetection.js';
-export { IPlatformDetectionService } from './common/platformDetection.js';
-export type { PlatformContext } from './common/platformDetection.js';
+export * from './common/connectorConfigStore.js';
 
 // Node implementations (main process only)
-export { ConnectorRegistryImpl } from './node/connectorRegistryImpl.js';
 export { MCPClientManagerImpl } from './node/mcpClientManagerImpl.js';
-export { CLIDetectionServiceImpl } from './node/cliDetectionImpl.js';
-export { MockCLIDetectionService } from './node/mockCLIDetection.js';
-export { PlatformDetectionServiceImpl } from './node/platformDetectionImpl.js';
+export * from './node/connectorConfigStore.js';
+export * from './node/agentTools.js';
